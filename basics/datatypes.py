93792b2f2f8be0f7d1cef28a898adcaffee2c331
vinothkumar-tw <vinothkumar.d@thoughtworks.com>
2025-06-27 21:12:02 +0530

    feat: add datatypes & operators

---
+++ basics/datatypes.py
@@ -0,0 +1,32 @@
+# numbers
+
+# integers
+a=12
+# float
+a=12.2
+# complex number
+a=12.2j
+
+# String
+str='hello'
+
+# Boolean
+is_running=False;
+is_running=True;
+
+# Lists
+a=['a','b']
+
+# Tuples
+
+a=('a',)
+
+# Sets
+
+s={'a','b'}
+
+# Dictionary
+
+ob={key: key for key in 'vinoth'}
+
+
---
+++ basics/operators.py
@@ -0,0 +1,20 @@
+# Arithmetic operators
+
+# Basics
+# +,-,*,/,//,%,**
+
+# assignment operators
+# =,+=,-=,*=,/=,//,**=,
+
+# logical operators
+# or, and, not
+
+# Identity operators
+# is, is not
+
+# Membership operator
+# in, in not
+
+# bitwise operator
+# &,|,^,~,<<,>>
+
